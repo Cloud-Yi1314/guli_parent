2c2428bd605a8bc218bbad52c258a464749a082c
CloudYi-1314 <ff1770236230@163.com>
2020-12-07 23:48:58 +0800

    day2 --统一返回格式

---
+++ common/common_util/src/main/java/com/atguigu/commonutils/ResultCode.java
@@ -0,0 +1,13 @@
+package com.atguigu.commonutils;
+
+/**
+ * @author YiCloud
+ * @date 2020/12/7 - 22:40
+ */
+public interface ResultCode {
+
+    public static Integer SUCCESS = 20000;  //成功
+
+    public static Integer ERROR = 20001;    //失败
+
+}
--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,8 +1,8 @@
 package com.atguigu.eduservice.controller;
-
-
+import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -33,18 +33,22 @@ public class EduTeacherController {
     //rest风格
     @ApiOperation(value="查询讲师表所有数据")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTecher(){
+    public R findAllTecher(){
         //调用service的方法实现查询所有
         List<EduTeacher> list = teacherService.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
 
     //2逻辑删除讲师的方法
     @ApiOperation(value="逻辑删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@ApiParam(name="id",value="讲师id",required = true) @PathVariable String id){
+    public R removeTeacher(@ApiParam(name="id",value="讲师id",required = true) @PathVariable String id){
         boolean flag = teacherService.removeById(id);
-        return true;
+        if(flag){
+            return R.ok();
+        }else{
+            return R.error();
+        }
     }
 }
 
