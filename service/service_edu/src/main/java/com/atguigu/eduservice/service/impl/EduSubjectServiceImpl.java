0515857a6231c46cabac30d30c824962bf14b96a
CloudYi-1314 <ff1770236230@163.com>
2020-12-23 23:16:06 +0800

    day7 -- 课程分类树形列表

--- service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -3,14 +3,20 @@ package com.atguigu.eduservice.service.impl;
 import com.alibaba.excel.EasyExcel;
 import com.atguigu.eduservice.entity.EduSubject;
 import com.atguigu.eduservice.entity.excel.SubjectData;
+import com.atguigu.eduservice.entity.subject.OneSubject;
+import com.atguigu.eduservice.entity.subject.TwoSubject;
 import com.atguigu.eduservice.listener.SubjectExcelListener;
 import com.atguigu.eduservice.mapper.EduSubjectMapper;
 import com.atguigu.eduservice.service.EduSubjectService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -34,5 +40,45 @@ public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubj
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public List<OneSubject> getAllOneTwoSubject() {
+        //1.查询所有一级分类 parentid=0
+        QueryWrapper<EduSubject> wrapperOne = new QueryWrapper<>();
+        wrapperOne.eq("parent_id","0");
+        List<EduSubject> oneSubjectList = this.list(wrapperOne);
+
+        //2.查询所有二级分类 parentid !=0
+        QueryWrapper<EduSubject> wrapperTwo = new QueryWrapper<>();
+        wrapperTwo.ne("parent_id","0");
+        List<EduSubject> twoSubjectList = this.list(wrapperTwo);
+
+        List<OneSubject> finalSubjectList = new ArrayList<>();
+        //3.封装一级分类
+        for (int i = 0; i < oneSubjectList.size(); i++) {
+            EduSubject eduSubject = oneSubjectList.get(i);
+            OneSubject oneSubject = new OneSubject();
+//            oneSubject.setId(eduSubject.getId());
+//            oneSubject.setTitle(eduSubject.getTitle());
+
+            BeanUtils.copyProperties(eduSubject,oneSubject);
+
+            finalSubjectList.add(oneSubject);
+
+            //4.封装二级分类
+            List<TwoSubject> twoFinalSubjectList = new ArrayList<>();
+            for (int j = 0; j < twoSubjectList.size(); j++) {
+                //获取每个二级分类
+                EduSubject tSubject = twoSubjectList.get(j);
+                if(tSubject.getParentId().equals(eduSubject.getId())){
+                    TwoSubject twoSubject = new TwoSubject();
+                    BeanUtils.copyProperties(tSubject,twoSubject);
+                    twoFinalSubjectList.add(twoSubject);
+                }
+            }
+            oneSubject.setChildren(twoFinalSubjectList);
+        }
+        return finalSubjectList;
     }
 }
